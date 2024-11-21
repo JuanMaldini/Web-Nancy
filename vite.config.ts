3e406661b2acb6fd8ff11bb19bdd4d57a4168525
Juan Maldini <juan.maldini@outlook.com>
2024-11-21 20:52:51 +0100

    up fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {resolve} from "path"
+import * as path from 'path';
+import * as url from "url";
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve:{
     alias:{
-      "@": resolve(__dirname, "src")
+      "@": path.resolve(__dirname, "src")
     }
   }
 })
