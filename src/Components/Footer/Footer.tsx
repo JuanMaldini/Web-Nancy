9a2abaee1f0f941aa6144e5347393cbba478afe7
Juan Maldini <juan.maldini@outlook.com>
2024-12-23 23:47:33 -0300

    up

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -13,39 +13,39 @@ export default function Footer({}: Props) {
       <div className="footerSection">
         <footer>
 
-          <div className="d-flex flex-row justify-content-between footerDiv">
+          <div className="colFooter footerDiv">
  
-            <ul className="d-flex flex-column col-3 justify-content-start list-unstyled">
+            <ul className="d-flex flex-column col-3 list-unstyled">
               <li><a href="#"><img src={ContactImg} alt="Image for Contact" width="100"/></a></li>
               <li>Buenos Aires, Argentina</li>
             </ul>
 
-            <ul className="nav d-flex flex-column col-2 justify-content-start">
-              <li className="fs-7 fst-italic nav-item-title">LINKS</li>
-              <li className="nav-item"><a href="#Navbar" className="linkUnderline">Inicio</a></li>
-              <li className="nav-item"><a href="#ProductsArea" className="linkUnderline">Productos</a></li>
-              <li className="nav-item"><a href="#GalleryArea" className="linkUnderline">Galería</a></li>
-              <li className="nav-item"><a href="#Contact" className="linkUnderline">Contacto</a></li>
+            <ul className="nav d-flex flex-column col-2">
+              <li className="fs-7 fst-italic">LINKS</li>
+              <li className=""><a href="#Navbar" className="linkUnderline">Inicio</a></li>
+              <li className=""><a href="#ProductsArea" className="linkUnderline">Productos</a></li>
+              <li className=""><a href="#GalleryArea" className="linkUnderline">Galería</a></li>
+              <li className=""><a href="#Contact" className="linkUnderline">Contacto</a></li>
             </ul>
 
-            <ul className="nav d-flex flex-column col-2 justify-content-start">
-              <li className="fs-7 fst-italic nav-item-title">AYUDA</li>
-              <li className="nav-item"><a href="#Faq" className="linkUnderline">FAQs</a></li>
+            <ul className="nav d-flex flex-column col-2">
+              <li className="fs-7 fst-italic">AYUDA</li>
+              <li className=""><a href="#Faq" className="linkUnderline">FAQ</a></li>
             </ul>
 
-            <ul className="nav d-flex flex-column col-3 justify-content-start align-items-start ">
-              <li className="fs-7 fst-italic nav-item-title">CONTACTO</li>
-              <ul className="nav d-flex flex-row justify-content-start">
-                <li className="nav-item mx-1"><a href={EmailLink} target="_blank"><IoMailOutline style={{ color: 'black' }} /></a></li>
-                <li className="nav-item mx-1"><a href={EmailLink} target="_blank" className="linkUnderline">Email</a></li>
+            <ul className="nav d-flex flex-column col-3">
+              <li className="fs-7 fst-italic">CONTACTO</li>
+              <ul className="nav">
+                <li className="mx-1"><a href={EmailLink} target="_blank"><IoMailOutline style={{ color: 'black' }} /></a></li>
+                <li className="mx-1"><a href={EmailLink} target="_blank" className="linkUnderline">Email</a></li>
               </ul>
-              <ul className="nav d-flex flex-row justify-content-start">
-                <li className="nav-item mx-1"><a href={InstagramLink} target="_blank"><FaInstagram style={{ color: 'black' }} /></a></li>
-                <li className="nav-item mx-1"><a href={InstagramLink} target="_blank" className="linkUnderline">Instagram</a></li>
+              <ul className="nav">
+                <li className="mx-1"><a href={InstagramLink} target="_blank"><FaInstagram style={{ color: 'black' }} /></a></li>
+                <li className="mx-1"><a href={InstagramLink} target="_blank" className="linkUnderline">Instagram</a></li>
               </ul>
-              <ul className="nav d-flex flex-row justify-content-start">
-                <li className="nav-item mx-1"><a href={getWhatsappContact()} target="_blank"><FaWhatsapp style={{ color: 'black' }} /></a></li>
-                <li className="nav-item mx-1"><a href={getWhatsappContact()} target="_blank" className="linkUnderline">Whatsapp</a></li>
+              <ul className="nav">
+                <li className="mx-1"><a href={getWhatsappContact()} target="_blank"><FaWhatsapp style={{ color: 'black' }} /></a></li>
+                <li className="mx-1"><a href={getWhatsappContact()} target="_blank" className="linkUnderline">Whatsapp</a></li>
               </ul>
             </ul>
           </div>
